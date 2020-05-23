0590546c44ff987449424847e933337079704f02
AishwaryaMurali1 <58639239+AishwaryaMurali1@users.noreply.github.com>
2020-05-22 21:23:07 -0700

    Add files via upload

---
+++ Activity2.java
@@ -0,0 +1,78 @@
+package com.example.numads20aishwaryamurali;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class Activity2 extends AppCompatActivity implements View.OnClickListener {
+    private Button button2,button3,button4,button5,button6,button7,button9;
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        button5 = findViewById(R.id.button5);
+        button6 = findViewById(R.id.button6);
+        button7 = findViewById(R.id.button7);
+        button9 = findViewById(R.id.button9);
+        tv = findViewById(R.id.textView2);
+
+        /*button = findViewById(R.id.button9);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });*/
+
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+        button7.setOnClickListener(this);
+        button9.setOnClickListener(this);
+
+    }
+
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.button2:
+                tv.setText("Pressed : A");
+                break;
+            case R.id.button3:
+                tv.setText("Pressed : B");
+                break;
+            case R.id.button4:
+                tv.setText("Pressed : C");
+                break;
+            case R.id.button5:
+                tv.setText("Pressed : D");
+                break;
+            case R.id.button6:
+                tv.setText("Pressed : E");
+                break;
+            case R.id.button7:
+                tv.setText("Pressed : F");
+                break;
+            case R.id.button9:
+                openMainActivity();
+                break;
+        }
+    }
+}
--- MainActivity.java
+++ MainActivity.java
@@ -1,60 +1,32 @@
 package com.example.numads20aishwaryamurali;
 
-import android.graphics.Color;
-import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.app.Activity;
-import android.widget.TextView;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
     }
 
-    /*public void send_task(View v){
-        TextView tv = (TextView) findViewById(R.id.textView);
-        Button btn = (Button) findViewById(R.id.button);
-        tv.setTextColor(Color.BLUE);
-        tv.setText("Aishwarya Murali  murali.ais@husky.neu.edu");
-        btn.setText("Go Back");
-
-    }*/
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void openActivity2(){
+        Intent intent = new Intent(this,Activity2.class);
+        startActivity(intent);
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 }
+
+
